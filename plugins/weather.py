ec515b9a89245d3d3a47d8f1e01e3ab6432641b9
Henry Yang <henryyang666@hotmail.com>
2024-04-03 20:01:34 +0800

    feat: 重写了weather插件，采用了新的api

--- plugins/weather.py
+++ plugins/weather.py
@@ -12,9 +12,7 @@ class weather(PluginInterface):
         with open(config_path, "r", encoding="utf-8") as f:  # 读取设置
             config = yaml.safe_load(f.read())
 
-        self.weather_api = config["weather_api"]
-        self.weather_appid = config["weather_appid"]
-        self.weather_appsecret = config["weather_appsecret"]
+        self.weather_api_key = config["weather_api_key"]
 
         main_config_path = "main_config.yml"
         with open(main_config_path, "r", encoding="utf-8") as f:  # 读取设置
@@ -25,46 +23,84 @@ class weather(PluginInterface):
         self.bot = pywxdll.Pywxdll(self.ip, self.port)  # 机器人api
 
     async def run(self, recv):
-        if len(recv["content"]) == 2:
-            city = recv["content"][1]  # 获取要查询的天气
-            url = f"{self.weather_api}?appid={self.weather_appid}&appsecret={self.weather_appsecret}&unescape=1&city={city}"  # 从设置中获取链接，密钥，并构成url
-            try:
+        error = ''
+        if len(recv['content']) != 2:
+            error = '指令格式错误！'
 
+        if not error:
+            # 首先请求geoapi，查询城市的id
+            request_city = recv['content'][1]
+            geo_api_url = f'https://geoapi.qweather.com/v2/city/lookup?key={self.weather_api_key}&number=1&location={request_city}'
+
+            conn_ssl = aiohttp.TCPConnector(verify_ssl=False)
+            async with aiohttp.request('GET', url=geo_api_url, connector=conn_ssl) as response:
+                geoapi_json = await response.json()
+                await conn_ssl.close()
+
+            if geoapi_json['code'] == '200':  # 如果城市存在
+                request_city_id = geoapi_json['location'][0]['id']
+                request_city_name = geoapi_json['location'][0]['name']
+
+                # 请求现在天气api
                 conn_ssl = aiohttp.TCPConnector(verify_ssl=False)
-                async with aiohttp.request("GET", url=url, connector=conn_ssl) as req:
-                    res = await req.json()
+                now_weather_api_url = f'https://devapi.qweather.com/v7/weather/now?key={self.weather_api_key}&location={request_city_id}'
+                async with aiohttp.request('GET', url=now_weather_api_url, connector=conn_ssl) as response:
+                    now_weather_api_json = await response.json()
+                    await conn_ssl.close()
 
-                if "city" in res.keys():
-                    out_message = f"-----XYBot-----\n城市🌆：{res['city']}\n天气☁️：{res['wea']}\n实时温度🌡️：{res['tem']}°\n白天温度🌡：{res['tem_day']}°\n夜晚温度🌡：{res['tem_night']}°\n空气质量🌬：{res['air']}\n空气湿度💦：{res['humidity']}\n风向🌬：{res['win_speed']}{res['win']}\n更新时间⌚：{res['update_time']}"  # 创建信息
-                    logger.info(f"[发送信息]{out_message}| [发送到] {recv['wxid']}")
-                    self.bot.send_txt_msg(recv["wxid"], out_message)
+                # 请求预报天气api
+                conn_ssl = aiohttp.TCPConnector(verify_ssl=False)
+                weather_forecast_api_url = f'https://devapi.qweather.com/v7/weather/7d?key={self.weather_api_key}&location={request_city_id}'
+                async with aiohttp.request('GET', url=weather_forecast_api_url, connector=conn_ssl) as response:
+                    weather_forecast_api_json = await response.json()
+                    await conn_ssl.close()
 
-                else:
-                    out_message = f"-----XYBot-----\n未知的城市：{city}❌"
-                    logger.info(f"[发送信息]{out_message}| [发送到] {recv['wxid']}")
-                    self.bot.send_txt_msg(recv["wxid"], out_message)
+                out_message = self.compose_weather_message(request_city_name, now_weather_api_json,
+                                                           weather_forecast_api_json)
+                self.send_friend_or_group(recv, out_message)
 
-            except KeyError as error:
-                error_args = error.args[0]
-                if error_args == "city":
-                    out_message = (
-                        f"-----XYBot-----\n未知的城市⚠️:{error_args}\n(仅支持国内城市！)"
-                    )
-                    logger.info(f"[发送信息]{out_message}| [发送到] {recv['wxid']}")
-                    self.bot.send_txt_msg(recv["wxid"], out_message)
+            elif geoapi_json['code'] == '404':
+                error = '-----XYBot-----\n⚠️城市不存在！'
+                self.send_friend_or_group(recv, error)
+            else:
+                error = f'-----XYBot-----\n⚠️请求失败！\n{geoapi_json}'
+                self.send_friend_or_group(recv, error)
 
-                else:
-                    out_message = f"-----XYBot-----\n出现错误！⚠️{error}"
-                    logger.info(f"[发送信息]{out_message}| [发送到] {recv['wxid']}")
-                    self.bot.send_txt_msg(recv["wxid"], out_message)
-
-
-            except Exception as error:  # 报错处理
-                out_message = f"-----XYBot-----\n出现错误！⚠️{error}"
-                logger.info(f'[发送信息]{out_message}| [发送到] {recv["wxid"]}')
-                self.bot.send_txt_msg(recv["wxid"], out_message)
 
         else:
-            out_message = "-----XYBot-----\n参数错误！⚠️"
-            logger.info(f"[发送信息]{out_message}| [发送到] {recv['wxid']}")
-            self.bot.send_txt_msg(recv["wxid"], out_message)
+            self.send_friend_or_group(recv, error)
+
+    def send_friend_or_group(self, recv, out_message="null"):
+        if recv["id1"]:  # 判断是群还是私聊
+            nickname = self.bot.get_chatroom_nickname(recv["wxid"], recv["id1"])["nick"]
+            logger.info(f'[发送信息]{out_message}| [发送到] {recv["wxid"]}')
+            self.bot.send_at_msg(
+                recv["wxid"], recv["id1"], nickname, "\n" + out_message
+            )  # 发送
+
+        else:
+            logger.info(f'[发送信息]{out_message}| [发送到] {recv["wxid"]}')
+            self.bot.send_txt_msg(recv["wxid"], out_message)  # 发送
+
+    def compose_weather_message(self, city_name, now_weather_api_json, weather_forecast_api_json):
+        update_time = now_weather_api_json['updateTime']
+        now_temperature = now_weather_api_json['now']['temp']
+        now_feelslike = now_weather_api_json['now']['feelsLike']
+        now_weather = now_weather_api_json['now']['text']
+        now_wind_direction = now_weather_api_json['now']['windDir']
+        now_wind_scale = now_weather_api_json['now']['windScale']
+        now_humidity = now_weather_api_json['now']['humidity']
+        now_precip = now_weather_api_json['now']['precip']
+        now_visibility = now_weather_api_json['now']['vis']
+        now_uvindex = weather_forecast_api_json['daily'][0]['uvIndex']
+
+        message = f'-----XYBot-----\n{city_name} 实时天气☁️\n更新时间：{update_time}⏰\n\n🌡️当前温度：{now_temperature}℃\n🌡️体感温度：{now_feelslike}℃\n☁️天气：{now_weather}\n☀️紫外线指数：{now_uvindex}\n🌬️风向：{now_wind_direction}\n🌬️风力：{now_wind_scale}级\n💦湿度：{now_humidity}%\n🌧️降水量：{now_precip}mm/h\n👀能见度：{now_visibility}km\n\n☁️未来3天 {city_name} 天气：\n'
+        for day in weather_forecast_api_json['daily'][1:4]:
+            date = '.'.join([i.lstrip('0') for i in day['fxDate'].split('-')[1:]])
+            weather = day['textDay']
+            max_temp = day['tempMax']
+            min_temp = day['tempMin']
+            uv_index = day['uvIndex']
+            message += f'{date} {weather} 最高🌡️{max_temp}℃ 最低🌡️{min_temp}℃ ☀️紫外线:{uv_index}\n'
+
+        return message
