e1b3fd904c9d82985e67acb9f3b79d4726362ef6
HenryXiaoYang <henryyang666@hotmail.com>
2024-05-15 18:08:41 +0800

    feat: 将pywxdll更新至v0.2.4

--- pywxdll/__init__.py
+++ pywxdll/__init__.py
@@ -1,4 +1,4 @@
 from .pywxdll import *
 from .pywxdll_json import *
 
-__version__ = '0.2'
+__version__ = '0.2.4'
--- pywxdll/pywxdll.py
+++ pywxdll/pywxdll.py
@@ -1,3 +1,4 @@
+import os
 import threading
 
 import requests
@@ -29,10 +30,10 @@ JOIN_ROOM = 10000
 
 class Pywxdll:
     def __init__(self, ip='127.0.0.1', port=5555):  # 微信hook服务器的ip地址和端口 The ip and port for wechat hook server
-        '''
+        """
         :param ip:
         :param port:
-        '''
+        """
         self.ip = ip
         self.port = port
         self._ws_url = f'ws://{ip}:{port}'  # websocket url
@@ -42,20 +43,20 @@ class Pywxdll:
 
     def _thread_start(self):  # 监听hook The thread for listeing
         websocket.enableTrace(False)  # 开启调试？
-        ws = websocket.WebSocketApp(
+        self.ws = websocket.WebSocketApp(
             self._ws_url,
             on_open=self._on_open,
             on_message=self._on_message,
             on_error=self._on_error,
             on_close=self._on_close
         )
-        ws.run_forever()
+        self.ws.run_forever()
 
     def start(self):  # 开始监听 Start listening for incoming message
-        '''
+        """
         开始监听微信消息 Start listening for incoming message
         :return:
-        '''
+        """
         wxt = threading.Thread(target=self._thread_start)
         wxt.daemon = True
         wxt.start()
@@ -68,13 +69,13 @@ class Pywxdll:
         r_type = recieve['type']
         if r_type == 5005:
             pass
-        elif r_type == 1 or r_type == 3 or r_type == 49:
+        else:
             self.msg_list.append(self._recv_txt_handle(recieve))
 
     def _on_error(self, ws, error):  # For websocket
         raise error
 
-    def _on_close(self, ws):  # For websocket
+    def _on_close(self, ws, status_code, close_msg):  # For websocket
         pass
 
     ######## Send ########
@@ -104,47 +105,47 @@ class Pywxdll:
         return rsp
 
     def send_txt_msg(self, wxid: str, content: str):
-        '''
+        """
         发送txt消息到朋友或群 Send txt message to a friend or chatroom
         :param wxid: wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了)或者群号(以@chatroom结尾) wechatid(start with wxid_) or groupchatid(end with@chatroom)
         :param content: 要发送的文本 content to send
         :return: Dictionary
-        '''
+        """
         uri = '/api/sendtxtmsg'
         self._web_api_data.update_data('sent_message_count', self._web_api_data.get_data()['sent_message_count'] + 1)
         return self._send_http(uri, json_send_txt_msg(wxid, content))
 
     def send_pic_msg(self, wxid: str, path: str):
-        '''
+        """
         发送图片信息发送txt消息到朋友或群 Send picture to a friend or chatroom
         :param wxid: wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了)或者群号(以@chatroom结尾) wechatid(start with wxid_) or groupchatid(end with@chatroom)
         :param path: 图片路径 path to picture
         :return:
-        '''
+        """
         uri = '/api/sendpic'
         self._web_api_data.update_data('sent_message_count', self._web_api_data.get_data()['sent_message_count'] + 1)
         return self._send_http(uri, json_send_pic_msg(wxid, path))
 
     def send_at_msg(self, roomid: str, wxid: str, nickname: str, content: str):
-        '''
+        """
         发送@信息到群  send @ message to chatroom
         :param roomid: 群号(以@chatroom结尾) groupchatid(end with@chatroom)
         :param wxid: 要@的人的wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了) wechatid(start with wxid_) of person you want to @
         :param nickname: 要@的人的昵称，可随意修改 nickname of person you want to @
         :param content: 要发送的文本 content to send
         :return:
-        '''
+        """
         uri = '/api/sendatmsg'
         self._web_api_data.update_data('sent_message_count', self._web_api_data.get_data()['sent_message_count'] + 1)
         return self._send_http(uri, json_send_at_msg(roomid, wxid, nickname, content))
 
     def send_attach_msg(self, wxid: str, path: str):
-        '''
+        """
         发送文件到朋友或群 send attachment to friend or chatroom
         :param wxid: wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了)或者群号(以@chatroom结尾) wechatid(start with wxid_) or groupchatid(end with@chatroom)
         :param path: 文件的路径 path to file
         :return:
-        '''
+        """
         uri = '/api/sendattatch'
         self._web_api_data.update_data('sent_message_count', self._web_api_data.get_data()['sent_message_count'] + 1)
         return self._send_http(uri, json_send_attach_msg(wxid, path))
@@ -152,53 +153,54 @@ class Pywxdll:
     ######## 获取信息 ########
 
     # 获取唯一id
-    def _getid(self):
+    @staticmethod
+    def _getid():
         return time.strftime("%Y%m%d%H%M%S", time.localtime())
 
     def heartbeat(h):
         return h
 
     def get_personal_detail(self, wxid: str):
-        '''
+        """
         获取其他账号信息 get other user's information
         :param wxid: wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了) wechatid(start with wxid_)
         :return: Dictionary
-        '''
+        """
         uri = '/api/get_personal_detail'
         return self._send_http(uri, json_get_personal_detail(wxid))['content']
 
     def get_contact_list(self):
-        '''
+        """
         获取微信通讯录用户名字和wxid get wechat address list username and wxid
         :return: Dictionary
-        '''
+        """
         uri = '/api/getcontactlist'
         return self._send_http(uri, json_get_contact_list())['content']
 
     def get_chatroom_nickname(self, roomid: str = 'null', wxid: str = 'ROOT'):
-        '''
+        """
         获取群聊中用户昵称 Get chatroom's user's nickname
         :param roomid: 群号(以@chatroom结尾) groupchatid(end with@chatroom)
         :param wxid: wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了) wechatid(start with wxid_)
         :return: Dictionary
-        '''
+        """
         uri = 'api/getmembernick'
         return self._send_http(uri, json_get_chatroom_nick(roomid, wxid))['content']
 
     def get_user_nickname(self, wxid: str):
-        '''
+        """
         获取朋友昵称 Get friend's nickname
         :param wxid: wxid(新用户的wxid以wxid_开头 老用户他们可能修改过 现在改不了) wechatid(start with wxid_)
         :return: Dictionary
-        '''
+        """
         return self.get_chatroom_nickname(wxid=wxid)
 
     def get_chatroom_memberlist(self, roomid: str = 'null'):
-        '''
+        """
         获取群聊中用户列表 Get chatroom member list
         :param roomid: 群号(以@chatroom结尾) groupchatid(end with@chatroom)
         :return: List or Dictionary
-        '''
+        """
         uri = '/api/get_charroom_member_list'
         result = self._send_http(uri, json_get_chatroom_memberlist(roomid))['content']
         if roomid == 'null':
@@ -211,5 +213,68 @@ class Pywxdll:
 
     ######## 信息处理 ########
 
-    def _recv_txt_handle(self, recieve):
+    @staticmethod
+    def _recv_txt_handle(recieve):
         return recieve
+
+    ######## 解密图片 ########
+
+    # 感谢群友提供的代码
+
+    @staticmethod
+    def _get_xor(file_buffer, suffix_map):
+        for key in suffix_map.keys():
+            suffix = suffix_map[key]
+            hex_values = [key[i:i + 2] for i in range(0, len(key), 2)]
+            map_values = []
+            for a in range(3):
+                byte = file_buffer[a]
+                value = byte ^ int(hex_values[a], 16)
+                map_values.append(value)
+            if map_values[0] == map_values[1] == map_values[2]:
+                return {"value": format(map_values[0], 'x'), "suffix": suffix}
+        return None
+
+    def decrypt_wechat_picture(self, file_path: str, output_dir: str = '') -> str:
+        if not os.path.isdir(output_dir) and output_dir:
+            raise Exception('Output directory does not exist')
+        elif not os.path.isfile(file_path):
+            raise Exception('File does not exist')
+
+        file_path = os.path.abspath(file_path)
+
+        suffix_map = {
+            'ffd8ffe000104a464946': 'jpg',
+            '89504e470d0a1a0a0000': 'png',
+            '47494638396126026f01': 'gif',
+            '49492a00227105008037': 'tif',
+            '424d228c010000000000': 'bmp',
+            '424d8240090000000000': 'bmp',
+            '424d8e1b030000000000': 'bmp'
+        }
+
+        with open(file_path, 'rb') as file:
+            buffer = file.read()
+
+        if buffer:
+            xor1 = self._get_xor(buffer, suffix_map)
+            if not xor1:
+                raise Exception('Decrypt failed')
+
+            # 转换之后的文件流
+            new_buffer = bytearray()
+            # 遍历文件流
+            for value in buffer:
+                # 异或运算
+                new_buffer.append(value ^ int('0x' + xor1['value'], 16))
+
+            # 保存文件
+            save_file_name = os.path.join(output_dir,
+                                          os.path.splitext(os.path.basename(file_path))[0] + '.' + xor1['suffix'])
+            with open(save_file_name, 'wb') as file:
+                file.write(new_buffer)
+
+            return os.path.abspath(save_file_name)
+        else:
+            # 处理读取失败的情况
+            raise Exception('Read file failed')
