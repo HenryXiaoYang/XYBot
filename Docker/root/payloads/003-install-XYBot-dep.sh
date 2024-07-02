7f3f30ec22219972d959e8187130ac4c8c8d363e
HenryXiaoYang <henryyang666@hotmail.com>
2024-07-02 17:12:57 +0800

    fix: pypi镜像源打错了

--- Docker/root/payloads/003-install-XYBot-dep.sh
+++ Docker/root/payloads/003-install-XYBot-dep.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/app/XYBot || exit
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
