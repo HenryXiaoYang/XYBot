6d977a20b200fdef82365b75a4db8886280ab430
HenryXiaoYang <henryyang666@hotmail.com>
2024-07-02 18:02:42 +0800

    fix: Docker build时git clone应该用app用户

--- Docker/root/payloads/002-clone-XYBot.sh
+++ Docker/root/payloads/002-clone-XYBot.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-cd /home/app || exit
-git clone https://github.com/HenryXiaoYang/XYBot.git
-cd XYBot || exit
-git checkout dev
+sudo -u app bash -c "cd /home/app || exit && git clone https://github.com/HenryXiaoYang/XYBot.git && cd XYBot || exit && git checkout dev"
