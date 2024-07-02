aaed4d391b828817e9a7dbee097a4e01ee62bc76
HenryXiaoYang <henryyang666@hotmail.com>
2024-07-02 18:16:20 +0800

    fix: 权限一直有问题

--- Docker/root/payloads/002-clone-XYBot.sh
+++ Docker/root/payloads/002-clone-XYBot.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sudo -u app bash -c "cd /home/app || exit && git clone https://github.com/HenryXiaoYang/XYBot.git && cd XYBot || exit && git checkout dev"
+cd /home/app || exit
+git clone https://github.com/HenryXiaoYang/XYBot.git
+cd XYBot || exit
+git checkout dev
