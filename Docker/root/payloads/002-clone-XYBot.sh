1d1f87dced592c57ac297774762021f94dca35f0
HenryXiaoYang <henryyang666@hotmail.com>
2024-07-02 17:08:23 +0800

    fix: fatal: not a git repository (or any of the parent directories): .git

--- Docker/root/payloads/002-clone-XYBot.sh
+++ Docker/root/payloads/002-clone-XYBot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cd /home/app || exit
-git clone https://github.com/HenryXiaoYang/XYBot
+git clone https://github.com/HenryXiaoYang/XYBot.git
+cd XYBot || exit
 git checkout dev
